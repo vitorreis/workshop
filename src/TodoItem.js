2e281b0c9d93053df5eabc208038368a9d52c6b4
Vitor Reis <vitor.reis@foodora.com>
2017-04-20 10:43:57 +0200

    implementation v1

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,3 +2,17 @@
  * This component should render a todo item as an <input type="checkbox" /> with a label
  */
 import React, { Component } from 'react';
+
+const TodoItem = (props) => {
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={props.checked}
+        readOnly={true} />
+      {props.text}
+    </label>
+  );
+};
+
+export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,3 +2,22 @@
  * This component should render the TodoItems inside a <li>
  */
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+const TodoList = (props) => {
+  return (
+    <ul>
+      {props.items.map((item) => {
+         return (
+           <li key={item.name}>
+            <TodoItem
+                      checked={item.checked}
+                      text={item.text} />
+          </li>
+          );
+        })}
+    </ul>
+  );
+};
+
+export default TodoList;
